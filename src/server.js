bd3b5a9e28177abbdde4d619f562da74b40774d3
Vipul S Sawant <vipulsawant847@gmail.com>
2026-03-20 18:01:08 +0530

    gracefull shutdown added

--- src/server.js
+++ src/server.js
@@ -12,10 +12,11 @@ const initiateServer = async () => {
     	logger.info("Database connected successfully");
 
 		const PORT = process.env.PORT;
-		app.listen(PORT, () => {
+		// app.listen(PORT, () => {
 
+			await new Promise((resolve) => app.listen(PORT, resolve));
 			logger.info({ port: PORT }, "Server running successfully");
-		});
+		// });
 	} catch (error) {
 		
 		logger.fatal({ err: error }, "Server failed to start");
@@ -24,3 +25,36 @@ const initiateServer = async () => {
 };
 
 initiateServer();
+
+const shutdown = async (signal) => {
+	logger.warn(`${signal} received. Shutting down gracefully...`);
+
+	try {
+		// Stop accepting new connections
+		server.close(() => {
+			logger.info("HTTP server closed");
+		});
+
+		// Close socket.io
+		if (io) {
+			io.close(() => {
+				logger.info("Socket.IO closed");
+			});
+		}
+
+		// Close DB
+		const mongoose = await import("mongoose");
+		await mongoose.default.connection.close();
+		logger.info("MongoDB connection closed");
+
+		process.exit(0);
+
+	} catch (err) {
+		logger.error({ err }, "Error during shutdown");
+		process.exit(1);
+	}
+};
+
+// Handle signals
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
