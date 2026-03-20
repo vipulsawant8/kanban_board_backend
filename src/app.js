fbe6219c90f21823cc5cb1c91e6789f123df499f
vipulsawant8 <67418646+vipulsawant8@users.noreply.github.com>
2026-03-20 15:41:05 +0530

    loggers updated (#2)

--- src/app.js
+++ src/app.js
@@ -21,11 +21,11 @@ app.disable('x-powered-by');
 app.set("trust proxy", true);
 
 const allowedOrigins = process.env.CORS_ORIGIN.split(',');
-logger.info({ allowedOrigins }, "CORS configuration loaded");
+logger.debug({ allowedOrigins }, "CORS configuration loaded");
 
 const corsOptions = {
 	origin: function (origin, callback) {
-		logger.debug({ requestOrigin: origin }, "Incoming CORS origin check");
+		logger.info({ requestOrigin: origin }, "Incoming CORS origin check");
 		if (!origin) return callback(null, true);
 		if (allowedOrigins.indexOf(origin) === -1) {
 			const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
